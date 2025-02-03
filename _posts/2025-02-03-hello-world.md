49f95263522c4387f5cef580192d7d1869c013b3
whoami <diogo.m.lages@gmail.com>
2025-02-03 13:47:08 +0000

    Post created test

---
+++ _posts/2025-02-03-hello-world.md
@@ -0,0 +1,5 @@
+---
+layout: post
+title: Hello World
+date: 2025-02-03 13:46 +0000
+---
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738511795',resources: [
+    cacheName: 'chirpy-1738590249',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
