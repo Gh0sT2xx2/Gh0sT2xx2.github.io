b86be2c7beadeacf37a2c01401e96a4368ebf0dc
whoami <diogo.m.lages@gmail.com>
2025-02-03 14:53:00 +0000

    new post

---
+++ _posts/2025-02-03-coming-soon.md
@@ -0,0 +1,5 @@
+---
+layout: post
+title: Coming soon
+date: 2025-02-03 14:48 +0000
+---
--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738593930',resources: [
+    cacheName: 'chirpy-1738594207',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
