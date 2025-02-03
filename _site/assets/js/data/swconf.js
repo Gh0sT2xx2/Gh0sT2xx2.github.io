6ed82bffd95c36344dd33979d7b6e054c778e8e6
whoami <diogo.m.lages@gmail.com>
2025-02-03 14:08:22 +0000

    Fix post setup in _config.yml

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738590249',resources: [
+    cacheName: 'chirpy-1738591650',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
