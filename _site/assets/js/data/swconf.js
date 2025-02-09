ce92ea524e7e2f791f4294eea0c846d3c9281127
whoami <diogo.m.lages@gmail.com>
2025-02-09 15:37:51 +0000

    config update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739115078',resources: [
+    cacheName: 'chirpy-1739115428',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
