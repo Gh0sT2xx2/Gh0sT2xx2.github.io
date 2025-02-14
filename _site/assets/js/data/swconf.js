7fc8a75a6c05ee684677e2a118c5de328516410d
whoami <diogo.m.lages@gmail.com>
2025-02-14 14:26:16 +0000

    new visual

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739542889',resources: [
+    cacheName: 'chirpy-1739543167',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
